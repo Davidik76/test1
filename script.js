4ee2450d01c0eeaa26d74a052cf65bfd1c2dec8d
Davidik76 <118114060+Davidik76@users.noreply.github.com>
2024-05-14 16:52:35 +0300

    Update script.js

--- script.js
+++ script.js
@@ -31,45 +31,30 @@ let clickCount = localStorage.getItem('clickCount') || 0;
 
 
     
-// const TOKEN = "6614663642:AAFrwsJiLTkDupbYfEhpgq2r753mVUCz3gY";
-// const CHAT_ID = "-1002115537261";
-// const URI_API = 'https://api.telegram.org/bot' + TOKEN + '/sendMessage';
+const TOKEN = "6614663642:AAFrwsJiLTkDupbYfEhpgq2r753mVUCz3gY";
+const CHAT_ID = "-1002115537261";
+const URI_API = 'https://api.telegram.org/bot' + TOKEN + '/sendMessage';
 
-// document.getElementById('tg').addEventListener('submit', function(e) {
-//   e.preventDefault();
-//   var val = document.getElementById('elem1').value;
-//   if (clickCount >= val && val >= 10) {
-//   clickCount-=val; 
-//   alert('Заявка успешно отправлена');
-//   localStorage.setItem('clickCount', clickCount);
-//   clickCountDisplay.textContent = `${clickCount}`; 
-//   localStorage.setItem('scoreprice', scoreprice);
+document.getElementById('tg').addEventListener('submit', function(e) {
+  e.preventDefault();
+  var val = document.getElementById('elem1').value;
+  if (clickCount >= val && val >= 10) {
+  clickCount-=val; 
+  alert('Заявка успешно отправлена');
+  localStorage.setItem('clickCount', clickCount);
+  clickCountDisplay.textContent = `${clickCount}`; 
+  localStorage.setItem('scoreprice', scoreprice);
   
-//   let message = '<b>Заявка с сайта!</b>';
+  let message = '<b>Заявка с сайта!</b>';
 
-//   axios.post(URI_API, {
-//     chat_id: CHAT_ID,
-//     parse_mode: 'html',
-//     text: message
-//   })
+  axios.post(URI_API, {
+    chat_id: CHAT_ID,
+    parse_mode: 'html',
+    text: message
+  })
 
-// } else {
-//   alert('Ошибка');
-// }
+} else {
+  alert('Ошибка');
+}
  
-// });
-
-
-
-const TelegramBot = require('node-telegram-bot-api');
-
-const token = '6614663642:AAFrwsJiLTkDupbYfEhpgq2r753mVUCz3gY';
-
-const bot = new TelegramBot(token, {polling: true});
-
-bot.onText(/\/start/, (msg) => {
-  const chatId = msg.chat.id;
-
-  bot.sendMessage(chatId, 'Hello');
 });
-
